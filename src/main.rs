9d50969e8273507d6edbf8f480a44410db970643
Stefan Utamaru <github.le5ke@stenyan.jp>
2024-11-19 17:17:17 +0900

    add structs for aggregated data

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,37 @@ struct LogEntry {
     response_time: String,
 }
 
+#[derive(Default)]
+struct AggregatedLogEntry {
+    method: String,
+    uri: String,
+    count: u64,
+    status_code: StatusCode,
+    response_time: ResponseTime,
+}
+
+#[derive(Default)]
+struct StatusCode {
+    status_1xx: u64,
+    status_2xx: u64,
+    status_3xx: u64,
+    status_4xx: u64,
+    status_5xx: u64,
+}
+
+#[derive(Default)]
+struct ResponseTime {
+    min: f64,
+    max: f64,
+    avg: f64,
+    sum: f64,
+    p50: f64,
+    p75: f64,
+    p90: f64,
+    p95: f64,
+    p99: f64,
+}
+
 fn main() {
     let stdin = io::stdin();
     let reader = stdin.lock();
