3c4f69b3aad0487656e6010734e674632eb4ea80
Stefan Utamaru <github.le5ke@stenyan.jp>
2024-11-19 13:46:28 +0900

    parse each line of access log

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::{self, BufRead};
+
+#[derive(serde::Deserialize, Debug)]
+struct LogEntry {
+    method: String,
+    uri: String,
+    status: String,
+    response_time: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let reader = stdin.lock();
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let entry: LogEntry = serde_json::from_str(&line).unwrap();
+        println!("{:?}", entry);
+    }
 }
