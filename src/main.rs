d55b932f6fdb0126f9586e1fcebd974099668855
Stefan Utamaru <github.le5ke@stenyan.jp>
2024-11-23 00:02:19 +0900

    divide parse_config_and_logs to 2 functions

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,22 @@ mod config;
 mod parse;
 
 use aggregate::aggregate_logs;
-use parse::parse_config_and_logs;
+use clap::Parser;
+use parse::{parse_config, parse_logs};
+
+/// A simple log aggregator that reads logs from stdin and outputs aggregated logs in JSON format.
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// Path to the config file
+    #[arg(short, long, default_value = "config.toml")]
+    config: String,
+}
 
 fn main() {
-    let (groupings, logs) = parse_config_and_logs();
+    let args = Args::parse();
+    let groupings = parse_config(&args.config);
+    let logs = parse_logs();
     let aggregated_logs = aggregate_logs(&logs, &groupings);
     let json_output = serde_json::to_string_pretty(&aggregated_logs).unwrap();
     println!("{}", json_output);
